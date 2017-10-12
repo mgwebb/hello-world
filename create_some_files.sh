7bcf2776a3dae4ecd703a9dcea3ab0aaf843e7d2
Mark Webb <mark.webb@cgi.com>
2017-10-12 00:02:04 -0400

    back to 3 files

--- create_some_files.sh
+++ create_some_files.sh
@@ -4,8 +4,5 @@ mkdir some-files
 echo "file1" > some-files/file1
 echo "file2" > some-files/file2
 echo "file3" > some-files/file3
-echo "file4" > some-files/file4
-echo "file5" > some-files/file5
-echo "file6" > some-files/file6
 
 ls some-files/*
