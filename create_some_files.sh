d9e26403abe49d36bd759236a299d277f5e3c73d
Mark Webb <mark.webb@cgi.com>
2017-10-11 23:39:29 -0400

    added two more files

--- create_some_files.sh
+++ create_some_files.sh
@@ -4,5 +4,7 @@ mkdir some-files
 echo "file1" > some-files/file1
 echo "file2" > some-files/file2
 echo "file3" > some-files/file3
+echo "file4" > some-files/file4
+echo "file5" > some-files/file5
 
 ls some-files/*
