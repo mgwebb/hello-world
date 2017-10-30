4ef16d30bcd6559deaaaf4b379715ae62091e58e
mgwebb <mark.webb@cgi.com>
2017-10-30 13:43:28 -0600

    fix syntax

--- show_files.sh
+++ show_files.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ls some-files/*
-echo Just showed files...now let's pause a few seconds...
+echo "Just showed files...now pause a few seconds..."
 sleep 10s
-echo Woke up from my 10 second nap.  Very refreshing!!
+echo "Woke up from my 10 second nap.  Very refreshing!!"
