2e9bb9505a6df5237aeb8836e6d0bca74dc36787
mgwebb <mark.webb@cgi.com>
2017-10-30 13:49:59 -0600

    increase sleep time

--- show_files.sh
+++ show_files.sh
@@ -2,5 +2,5 @@
 
 ls some-files/*
 echo "Just showed files...now pause a few seconds..."
-sleep 10s
-echo "Woke up from my 10 second nap.  Very refreshing!!"
+sleep 30s
+echo "Woke up from my 30 second nap.  Very refreshing!!"
